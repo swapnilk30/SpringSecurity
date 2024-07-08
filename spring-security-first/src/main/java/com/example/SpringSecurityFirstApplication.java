ffd5fe154fb9fd82ad1780faeda5aaa8b98e03de
swapnilk30 <sk9137587676@gmail.com>
2024-07-08 10:29:08 +0530

    first setup

---
+++ spring-security-first/src/main/java/com/example/SpringSecurityFirstApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityFirstApplication.class, args);
+	}
+
+}
